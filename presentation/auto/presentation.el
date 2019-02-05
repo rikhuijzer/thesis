edc531f95836e9905f6c943ca1999d018cc88354
Rik <t.h.huijzer@student.tue.nl>
2019-02-05 13:34:23 +0100

    introduced colors for NLP tasks

--- presentation/auto/presentation.el
+++ presentation/auto/presentation.el
@@ -3,17 +3,29 @@
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("beamer" "pdf")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("montserrat" "defaultfam" "tabular" "lining") ("fontenc" "T1")))
    (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
     "beamer"
-    "beamer10"))
+    "beamer10"
+    "montserrat"
+    "fontenc")
+   (TeX-add-symbols
+    '("pblue" 1)
+    '("pgreen" 1)
+    '("blue" 1)
+    '("green" 1))
+   (LaTeX-add-xcolor-definecolors
+    "ao(english)"
+    "azure(colorwheel)"))
  :latex)
 
