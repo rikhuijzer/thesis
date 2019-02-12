c3a74af7aa529a7ddc19c16ee3793a006a0ace2e
Rik <t.h.huijzer@student.tue.nl>
2019-02-12 11:36:17 +0100

    various changes to presentation

---
+++ chapters/benchmarking/auto/benchmark_results.el
@@ -0,0 +1,12 @@
+(TeX-add-style-hook
+ "benchmark_results"
+ (lambda ()
+   (LaTeX-add-labels
+    "sec:bench"
+    "subsec:overview"
+    "fig:types"
+    "fig:flowchart"
+    "subsec:benchmark_results"
+    "tab:benchmark_comparison"))
+ :latex)
+
