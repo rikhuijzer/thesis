a3ede90009b3818c0b8a60bdd2f3454a61203c28
Rik <t.h.huijzer@student.tue.nl>
2019-02-13 20:58:39 +0100

    added decapitalized snips runs

---
+++ appendices/auto/runs.el
@@ -0,0 +1,11 @@
+(TeX-add-style-hook
+ "runs"
+ (lambda ()
+   (LaTeX-add-labels
+    "ch:runs"
+    "tab:runs_webapplications"
+    "tab:runs_askubuntu"
+    "tab:runs_chatbot"
+    "tab:runs_snips"))
+ :latex)
+
